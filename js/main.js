9367dec35e58bd31609e3b5d7caa7ac830411609
ArielAlegre12 <114325019+ArielAlegre12@users.noreply.github.com>
2026-03-24 10:10:30 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import { setupAccordion } from './accordion.js';
-import { setupTrivia } from './trivia.js';
-import { setupMenu } from './menu.js';
-import { setupDarkMode } from './darkmode.js';
-import { setupAcertijos } from './acertijos.js';
+import { setupAccordion } from '.accordion.js';
+import { setupTrivia } from '.trivia.js';
+import { setupMenu } from '.menu.js';
+import { setupDarkMode } from '.darkmode.js';
+import { setupAcertijos } from '.acertijos.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   setupAccordion();
